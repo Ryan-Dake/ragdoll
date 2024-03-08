3fd6fe88ecf0d037622b85dc773d11498d2e5a45
Ryan Dake <codingbear@protonmail.com>
2024-03-08 17:25:31 -0600

    Initial commit

---
+++ src/main/java/com/dake/ragdoll/RagdollApplication.java
@@ -0,0 +1,13 @@
+package com.dake.ragdoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagdollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagdollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dake/ragdoll/RagdollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dake.ragdoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagdollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
